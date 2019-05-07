91436f357a9c1fd1603b7eef4a3b0e699e88b381
mcumm64 <mcumm64@gmail.com>
2019-05-07 20:27:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(line) 
+  if line.size == 0
+    puts "The line is currently empty."
+  else 
+    ordered_list = line.map.each_with_index do |person, index|
+    person = "#{index+1}. #{person}"
+    end
+    puts "The line is currently #{line.join(", ")}."
+  end
+end
