3ae4a83278cdc4365eaaadac6cadb1560a42a8de
mcumm64 <mcumm64@gmail.com>
2019-05-07 20:34:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,11 @@ def take_a_number(line, name)
   line << name
   puts "Welcome, #{name}. You are number #{line.index(name)+1} in line."
 end
+
+def now_serving(line)
+  if line.size == 0
+    puts "There is nobody waiting to be served!"
+  else 
+    puts line.pop
+  end
+end
